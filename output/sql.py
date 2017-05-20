6f2bca14ea57623b5574a09e8eceb149e10d3a4a
yfsoftcom <yfsoftcom@126.com>
2017-05-20 17:46:18 +0800

    add logs

--- output/sql.py
+++ output/sql.py
@@ -2,19 +2,30 @@
 
 import json
 import sys
+import requests
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 def generate_brand(items):
-    sql = 'insert into fpm_car_brand(id, name, firstletter) values '
+    sql = 'insert into fpm_car_brand(id, name, firstletter, logo) values '
     vals = []
     for id, item in items:
-        val = '(' + str(id) +  ',' + item['name'] + "', '" + item['firstletter'] + "'" + ')'
+        val = "( %d, '%s', '%s', '%s')" % (int(id), item['name'], item['firstletter'], item['logo'])
         vals.append(val)
     sql = sql + ','.join(vals)
     f = open('brand.sql', 'w')
     f.write(sql)
     f.close()
+
+def download_brand(items):
+    for id, item in items:
+        url = item['logo']
+        print url
+        r = requests.get(url, stream=True)
+        r.raw
+        f = open('%s.jpg' % str(id), 'wb')
+        f.write(r.content)
+        f.close()
 
 def generate_series(items):
     sql = 'insert into fpm_car_series(id, pid, name, firstletter) values '
@@ -93,13 +104,16 @@ if __name__ == '__main__':
     # brand  --------
     # generate_brand(obj['brand'].items())
     # print 'brand ok'
+    # download brand logos
+    download_brand(obj['brand'].items())
+    print 'download_brand ok'
     # generate_series(obj['series'].items())
     # print 'series ok'
     # generate_sub_series(obj['sub_series'].items())
     # print 'sub_series ok'
     # generate_year(obj['year'].items())
     # print 'year ok'
-    print len(obj['spec'])
-    generate_spec(obj['spec'].items())
-    print 'spec ok'
+    # print len(obj['spec'])
+    # generate_spec(obj['spec'].items())
+    # print 'spec ok'
 
